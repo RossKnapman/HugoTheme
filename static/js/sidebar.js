bd999d462041f3b99e5836397d7bff8ea0ab863a
Ross Knapman <rjknapman@gmail.com>
2021-10-31 13:35:39 +0100

    Can now click on overlay to close sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,4 +1,14 @@
-$('.toggle-sidebar').on('click', function () {
-    $('#sidebar').toggleClass('active');
-    $('.overlay').toggleClass('active');
+$('#open-sidebar').on('click', function () {
+    toggleSidebar();
+    $('#overlay')[0].addEventListener('click', toggleSidebar);
 });
+
+$('#close-sidebar').on('click', function () {
+    toggleSidebar();
+    $('#overlay')[0].removeEventListener('click', toggleSidebar);
+});
+
+function toggleSidebar() {
+    $('#sidebar').toggleClass('active');
+    $('#overlay').toggleClass('active');
+}
